6d38697af949fe04eaf86b51d0b2203fdd146270
javieravila0urbiport <javier.avila@urbiport.com>
2025-10-21 14:36:03 -0300

    fix: export WorkspaceBillingPlanTier and WorkspaceBillingPlan types
    - Add export type for WorkspaceBillingPlanTier and WorkspaceBillingPlan from @prisma/client
    - Re-enable WorkspaceRole, CollaborationType, BillingPlanType exports
    - This allows other packages to import these types from @quickbot.io/prisma
    - Fixes TypeScript errors in schemas/features/billing/plan.ts

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,5 +1,7 @@
 export * from '@prisma/client'
 
 // Named export for enums to avoid vite barrel export bug (https://github.com/nrwl/nx/issues/13704)
-// Temporarily commented out due to build issues - these types are available via export * above
-// export { WorkspaceRole, CollaborationType, BillingPlanType } from '@prisma/client'
+export { WorkspaceRole, CollaborationType, BillingPlanType } from '@prisma/client'
+
+// Export model types that are needed by other packages
+export type { WorkspaceBillingPlanTier, WorkspaceBillingPlan } from '@prisma/client'
