2a9db32ed715b20788e68a1261ae822df3696443
javieravila0urbiport <javier.avila@urbiport.com>
2025-10-21 14:53:37 -0300

    fix: temporarily comment out problematic Prisma enum exports
    - Comment out WorkspaceRole, CollaborationType, BillingPlanType exports
    - These types are still available via export * from '@prisma/client'
    - Keep WorkspaceBillingPlanTier and WorkspaceBillingPlan type exports
    - Fixes TypeScript error: Module '@prisma/client' has no exported member 'WorkspaceRole'
    - This is a temporary fix to unblock the build process

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,7 +1,8 @@
 export * from '@prisma/client'
 
 // Named export for enums to avoid vite barrel export bug (https://github.com/nrwl/nx/issues/13704)
-export { WorkspaceRole, CollaborationType, BillingPlanType } from '@prisma/client'
+// Temporarily commented out due to build issues - these types are available via export * above
+// export { WorkspaceRole, CollaborationType, BillingPlanType } from '@prisma/client'
 
 // Export model types that are needed by other packages
 export type { WorkspaceBillingPlanTier, WorkspaceBillingPlan } from '@prisma/client'
